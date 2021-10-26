2f12cfb75da5be8325dbd961e68512eff4885da8
HyunSang Park <me@hyunsang.dev>
2021-10-26 11:50:50 +0900

    Initial Commit

---
+++ AppStore/AppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreApp.swift
+//  AppStore
+//
+//  Created by HyunSang Park on 2021/10/26.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
